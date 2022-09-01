a3ba140330525921da87604cc97c1b038476c96f
Uriel Fiori <uriel.fiori@gmail.com>
2022-09-01 10:19:38 -0300

    added while loop to control user input

--- blockchain.py
+++ blockchain.py
@@ -23,18 +23,22 @@ def add_value(transaction_amount, last_transaction_value):
 def get_user_input():
     """gets user input about transaction amount
     """
-    tx_amount = float(input("Type the value of the transaction: "))
+    input_text = input("Type the value of the transaction: ")
+    if input_text == "exit":
+        return input_text
+    tx_amount = float(input_text)
     if len(blockchain) <= 0:
         add_value(tx_amount, [1.0])
     else:
         add_value(tx_amount, get_last_blockchain_value())
+    return "done"
 
 
-for item in range(3):
-    get_user_input()
+COMMAND = ""
+while COMMAND != "exit":
+    COMMAND = get_user_input()
 
-for block in blockchain:
-    print("Outputting block")
-    print(block)
-
-print("DONE!")
+    for block in blockchain:
+        print("Outputting block")
+        print(block)
+    print("DONE!")
