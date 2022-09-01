6f51f9f667b46cbe53c6d095a6bbfaf3e9980e25
Uriel Fiori <uriel.fiori@gmail.com>
2022-09-01 11:59:49 -0300

    improved CLI interface

--- blockchain.py
+++ blockchain.py
@@ -20,25 +20,63 @@ def add_value(transaction_amount, last_transaction_value):
     blockchain.append([last_transaction_value, transaction_amount])
 
 
-def get_user_input():
-    """gets user input about transaction amount
+def add_transaction():
+    """gets user input
     """
     input_text = input("Type the value of the transaction: ")
-    if input_text == "exit":
-        return input_text
-    tx_amount = float(input_text)
+
+    try:
+        tx_amount = float(input_text)
+        print("\n\nAdding a " + str(tx_amount) +
+              " transaction to the blockchain...\n\n")
+    except ValueError:
+        print("ERROR: That is not a number!")
+        input()
+        return
+
     if len(blockchain) <= 0:
         add_value(tx_amount, [1.0])
     else:
         add_value(tx_amount, get_last_blockchain_value())
-    return "done"
+    print("DONE!")
+    input()
+
+
+def get_user_choice():
+    """Get the user choice program choice
+
+    Returns:
+        string: the choice the user entered
+    """
+    return input("Your choice: ")
+
+
+def print_blockchain_blocks():
+    """Prints all the blocks in the blockchain
+    """
+    for block in blockchain:
+        print("Outputting block...")
+        print(block)
+    print("DONE!")
+    input()
 
 
 COMMAND = ""
 while COMMAND != "exit":
-    COMMAND = get_user_input()
+    print("\nChoose the command you want to execute...")
+    print("EXIT: exits the program")
+    print("TRANS: includes a new transaction in the blockchain")
+    print("PRINT: prints all the blocks in the blockchain\n")
+    COMMAND = get_user_choice()
 
-    for block in blockchain:
-        print("Outputting block")
-        print(block)
-    print("DONE!")
+    print("You chose: " + COMMAND + "\n")
+
+    match COMMAND:
+        case "exit" | "EXIT":
+            continue
+        case "trans" | "TRANS":
+            add_transaction()
+        case "print" | "PRINT":
+            print_blockchain_blocks()
+        case _:
+            print("That's not a command. Try again")
