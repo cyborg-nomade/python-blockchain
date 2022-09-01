1c5b3680db569124024b963ef9bf4315464d7381
Uriel Fiori <uriel.fiori@gmail.com>
2022-09-01 16:57:16 -0300

    refactored add transaction func

--- blockchain.py
+++ blockchain.py
@@ -6,28 +6,31 @@ blockchain = []
 def get_last_blockchain_value():
     """gets last blockchain value
 
-    Args:
-        blockchain (list[float]): the blockchain list[float]
-
     Returns:
         float: the last blockchain value
     """
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction_value):
-    """add value to blockchain"""
+def add_transaction(transaction_amount, last_transaction_value):
+    """add transaction to blockchain
+
+    Args:
+      transaction_amount (int): the amount to add to the blockchain
+      last_transaction_value (list): last blockchain value
+    """
+
+    if last_transaction_value is None:
+        last_transaction_value = [1.0]
     blockchain.append([last_transaction_value, transaction_amount])
 
 
 def get_user_input():
-    """gets user input about transaction amount
-    """
+    """gets user input about transaction amount"""
     tx_amount = float(input("Type the value of the transaction: "))
-    if len(blockchain) <= 0:
-        add_value(tx_amount, [1.0])
-    else:
-        add_value(tx_amount, get_last_blockchain_value())
+    add_transaction(tx_amount, get_last_blockchain_value())
 
 
 for item in range(3):
